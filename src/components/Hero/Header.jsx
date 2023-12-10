0fdc0a146708a322911594cc44c2df3c26832329
killer-goose <152170803+killer-goose@users.noreply.github.com>
2023-12-10 19:08:44 +0530

    triangle on top

--- src/components/Hero/Header.jsx
+++ src/components/Hero/Header.jsx
@@ -10,6 +10,7 @@ export default function Header() {
       <a href='#' id='logo' className='me-auto'>
         <img src='/images/logo.svg' alt='logo' />
       </a>
+      {/* mobile nav */}
       <nav id='mobile-nav' className='flex md:hidden'>
         <div className='group'>
           <img
@@ -17,8 +18,10 @@ export default function Header() {
             alt='hamburger icon'
             className='cursor-pointer'
           />
-          <div className='submenu absolute inset-0 top-10 z-10 hidden group-hover:block'>
-            <div className=' flex w-full flex-col gap-8 bg-white py-12 text-center font-bold text-neutral-blue-300'>
+          {/* Hack for hover. Checkout later. Use border */}
+          <div className='submenu absolute inset-0 top-8 z-10 hidden  h-max pt-16 group-hover:block'>
+            <div className='relative flex w-full flex-col gap-8 bg-white py-7 text-center font-bold text-neutral-blue-300'>
+              <div className='triangle absolute -top-[12%] left-[93.2%]'></div>
               {navElements.map((x, i) => (
                 <p key={i}>
                   <CTA
@@ -35,6 +38,7 @@ export default function Header() {
           </div>
         </div>
       </nav>
+      {/* desktop nav */}
       <nav id='desktop-nav' className='hidden font-semibold md:flex'>
         {navElements.map((x, i) => (
           <CTA
